39e9376b64ff08e5064330783ebecf9a907b6073
Siaoen <104399210@students.swinburne.edu.my>
2026-06-02 15:57:19 +0800

    server.js final

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,7 +40,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
-app.options('*', (req, res) => {
+app.options('/api/*', (req, res) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin)
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
