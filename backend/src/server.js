9227999c9bda3374129fbb75e2c491d104eed224
Siaoen <104399210@students.swinburne.edu.my>
2026-06-02 17:17:36 +0800

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -62,12 +62,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*')
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    return res.sendStatus(204)
-  }
+  console.log("Incoming Origin:", req.headers.origin)
   next()
 })
 
