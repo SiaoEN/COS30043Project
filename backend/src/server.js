8e9e0e2aab7d4e02541fff18d01906179800c6a0
Siaoen <104399210@students.swinburne.edu.my>
2026-06-02 17:06:37 +0800

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -61,7 +61,15 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
-app.options('/.*/', cors(corsOptions))
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*')
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    return res.sendStatus(204)
+  }
+  next()
+})
 
 app.use(express.json({ limit: '15mb' }))
 app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')))
