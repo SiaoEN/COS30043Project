360aaa4732d54b1f229ccc38bad7d2ff43423bc2
Siaoen <104399210@students.swinburne.edu.my>
2026-06-02 17:03:31 +0800

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -61,7 +61,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
-app.options('*', cors(corsOptions))
+app.options('/.*/', cors(corsOptions))
 
 app.use(express.json({ limit: '15mb' }))
 app.use('/uploads', express.static(path.resolve(__dirname, '../uploads')))
